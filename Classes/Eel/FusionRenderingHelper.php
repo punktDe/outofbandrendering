e30bc3aa9c8ecaaaa2d2cbeb1a0022ae816893d9
Daniel Lienert <daniel@lienert.cc>
2017-03-15 15:17:46 +0100

    TASK: Rewtite FusionRenderingHelper to use FusionRuntime

--- Classes/Eel/FusionRenderingHelper.php
+++ Classes/Eel/FusionRenderingHelper.php
@@ -9,9 +9,13 @@ namespace PunktDe\OutOfBandRendering\Eel;
 use Neos\ContentRepository\Domain\Model\NodeInterface;
 use Neos\Eel\ProtectedContextAwareInterface;
 use Neos\Flow\Annotations as Flow;
+use Neos\Fusion\Core\Runtime;
 use Neos\Neos\Controller\CreateContentContextTrait;
 use Neos\Flow\Http;
 use Neos\Flow\Mvc;
+use Neos\Flow\I18n\Service;
+use Neos\Flow\I18n\Locale;
+use Neos\Neos\Domain\Service\FusionService;
 
 class FusionRenderingHelper implements ProtectedContextAwareInterface
 {
@@ -20,9 +24,25 @@ class FusionRenderingHelper implements ProtectedContextAwareInterface
 
     /**
      * @Flow\Inject
-     * @var \Neos\Neos\View\FusionView
+     * @var Service
      */
-    protected $fusionView;
+    protected $i18nService;
+
+    /**
+     * @var Runtime
+     */
+    protected $fusionRuntime;
+
+    /**
+     * @Flow\Inject
+     * @var FusionService
+     */
+    protected $fusionService;
+
+    /**
+     * @var array
+     */
+    protected $options = ['enableContentCache' => true];
 
     public function initializeObject() {
         // we want to have nice URLs
@@ -36,15 +56,28 @@ class FusionRenderingHelper implements ProtectedContextAwareInterface
      */
     public function render(NodeInterface $node, $fusionPath)
     {
-        $controllerContext = $this->buildControllerContext();
-        $contentContext = $this->createContentContext('live', []);
-        $renderableNode = $contentContext->getNodeByIdentifier($node->getIdentifier());
+        $contentContext = $this->createContentContext('live', $node->getDimensions());
+        $currentNode = $contentContext->getNodeByIdentifier($node->getIdentifier());
 
-        $this->fusionView->setControllerContext($controllerContext);
+        $currentSiteNode = $currentNode->getContext()->getCurrentSiteNode();
+        $fusionRuntime = $this->getFusionRuntime($currentSiteNode);
 
-        $this->fusionView->setFusionPath($fusionPath);
-        $this->fusionView->assign('value', $renderableNode);
-        return $this->fusionView->render();
+        $dimensions = $currentNode->getContext()->getDimensions();
+        if (array_key_exists('language', $dimensions) && $dimensions['language'] !== []) {
+            $currentLocale = new Locale($dimensions['language'][0]);
+            $this->i18nService->getConfiguration()->setCurrentLocale($currentLocale);
+            $this->i18nService->getConfiguration()->setFallbackRule(['strict' => false, 'order' => array_reverse($dimensions['language'])]);
+        }
+
+        $fusionRuntime->pushContextArray([
+            'node' => $currentNode,
+            'documentNode' => $this->getClosestDocumentNode($currentNode) ?: $currentNode,
+            'site' => $currentSiteNode,
+            'editPreviewMode' => null
+        ]);
+        $output = $fusionRuntime->render($fusionPath);
+        $fusionRuntime->popContext();
+        return $output;
     }
 
 
@@ -70,5 +103,33 @@ class FusionRenderingHelper implements ProtectedContextAwareInterface
             new Mvc\Controller\Arguments(),
             new Mvc\Routing\UriBuilder()
         );
+    }
+
+    /**
+     * @param NodeInterface $currentSiteNode
+     * @return Runtime
+     */
+    protected function getFusionRuntime(NodeInterface $currentSiteNode)
+    {
+        if ($this->fusionRuntime === null) {
+            $this->fusionRuntime = $this->fusionService->createRuntime($currentSiteNode, $this->buildControllerContext());
+
+            if (isset($this->options['enableContentCache']) && $this->options['enableContentCache'] !== null) {
+                $this->fusionRuntime->setEnableContentCache($this->options['enableContentCache']);
+            }
+        }
+        return $this->fusionRuntime;
+    }
+
+    /**
+     * @param NodeInterface $node
+     * @return NodeInterface
+     */
+    protected function getClosestDocumentNode(NodeInterface $node)
+    {
+        while ($node !== null && !$node->getNodeType()->isOfType('Neos.Neos:Document')) {
+            $node = $node->getParent();
+        }
+        return $node;
     }
 }
