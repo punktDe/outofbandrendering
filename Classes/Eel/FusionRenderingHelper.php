2d0d7fbdda47b6502ea67d35229623bc223da881
Florian Kaiser <kaiser@punkt.de>
2017-11-22 08:34:04 +0100

    TASK: Move FusionRenderingHelper to FusionRenderingService, support custom context data

--- Classes/Eel/FusionRenderingHelper.php
+++ Classes/Eel/FusionRenderingHelper.php
@@ -1,53 +1,18 @@
 <?php
 namespace PunktDe\OutOfBandRendering\Eel;
 
-/*
- *  (c) 2017 punkt.de GmbH - Karlsruhe, Germany - http://punkt.de
- *  All rights reserved.
- */
-
 use Neos\ContentRepository\Domain\Model\NodeInterface;
 use Neos\Eel\ProtectedContextAwareInterface;
 use Neos\Flow\Annotations as Flow;
-use Neos\Fusion\Core\Runtime;
-use Neos\Neos\Controller\CreateContentContextTrait;
-use Neos\Flow\Http;
-use Neos\Flow\Mvc;
-use Neos\Flow\I18n\Service;
-use Neos\Flow\I18n\Locale;
-use Neos\Neos\Domain\Service\FusionService;
+use PunktDe\OutOfBandRendering\Service\FusionRenderingService;
 
 class FusionRenderingHelper implements ProtectedContextAwareInterface
 {
-
-    use CreateContentContextTrait;
-
     /**
      * @Flow\Inject
-     * @var Service
+     * @var FusionRenderingService
      */
-    protected $i18nService;
-
-    /**
-     * @var Runtime
-     */
-    protected $fusionRuntime;
-
-    /**
-     * @Flow\Inject
-     * @var FusionService
-     */
-    protected $fusionService;
-
-    /**
-     * @var array
-     */
-    protected $options = ['enableContentCache' => true];
-
-    public function initializeObject() {
-        // we want to have nice URLs
-        putenv('FLOW_REWRITEURLS=1');
-    }
+    protected $fusionRenderingService;
 
     /**
      * @param NodeInterface $node
@@ -56,30 +21,8 @@ class FusionRenderingHelper implements ProtectedContextAwareInterface
      */
     public function render(NodeInterface $node, $fusionPath)
     {
-        $contentContext = $this->createContentContext('live', $node->getDimensions());
-        $currentNode = $contentContext->getNodeByIdentifier($node->getIdentifier());
-
-        $currentSiteNode = $currentNode->getContext()->getCurrentSiteNode();
-        $fusionRuntime = $this->getFusionRuntime($currentSiteNode);
-
-        $dimensions = $currentNode->getContext()->getDimensions();
-        if (array_key_exists('language', $dimensions) && $dimensions['language'] !== []) {
-            $currentLocale = new Locale($dimensions['language'][0]);
-            $this->i18nService->getConfiguration()->setCurrentLocale($currentLocale);
-            $this->i18nService->getConfiguration()->setFallbackRule(['strict' => false, 'order' => array_reverse($dimensions['language'])]);
-        }
-
-        $fusionRuntime->pushContextArray([
-            'node' => $currentNode,
-            'documentNode' => $this->getClosestDocumentNode($currentNode) ?: $currentNode,
-            'site' => $currentSiteNode,
-            'editPreviewMode' => null
-        ]);
-        $output = $fusionRuntime->render($fusionPath);
-        $fusionRuntime->popContext();
-        return $output;
+        return $this->fusionRenderingService->render($node, $fusionPath);
     }
-
 
     /**
      * @param string $methodName
@@ -90,49 +33,4 @@ class FusionRenderingHelper implements ProtectedContextAwareInterface
         return true;
     }
 
-    /**
-     * @return Mvc\Controller\ControllerContext
-     */
-    protected function buildControllerContext()
-    {
-        $schemeAndHost = 'https://punkt.de/';
-
-        $httpRequest = Http\Request::create(new Http\Uri($schemeAndHost));
-        $httpRequest->setBaseUri(new Http\Uri($schemeAndHost));
-
-        return new Mvc\Controller\ControllerContext(
-            new Mvc\ActionRequest($httpRequest),
-            new Http\Response(),
-            new Mvc\Controller\Arguments(),
-            new Mvc\Routing\UriBuilder()
-        );
-    }
-
-    /**
-     * @param NodeInterface $currentSiteNode
-     * @return Runtime
-     */
-    protected function getFusionRuntime(NodeInterface $currentSiteNode)
-    {
-        if ($this->fusionRuntime === null) {
-            $this->fusionRuntime = $this->fusionService->createRuntime($currentSiteNode, $this->buildControllerContext());
-
-            if (isset($this->options['enableContentCache']) && $this->options['enableContentCache'] !== null) {
-                $this->fusionRuntime->setEnableContentCache($this->options['enableContentCache']);
-            }
-        }
-        return $this->fusionRuntime;
-    }
-
-    /**
-     * @param NodeInterface $node
-     * @return NodeInterface
-     */
-    protected function getClosestDocumentNode(NodeInterface $node)
-    {
-        while ($node !== null && !$node->getNodeType()->isOfType('Neos.Neos:Document')) {
-            $node = $node->getParent();
-        }
-        return $node;
-    }
 }
---
+++ Classes/Service/FusionRenderingService.php
@@ -0,0 +1,132 @@
+<?php
+namespace PunktDe\OutOfBandRendering\Service;
+
+use Neos\ContentRepository\Domain\Model\NodeInterface;
+use Neos\Flow\Annotations as Flow;
+use Neos\Fusion\Core\Runtime;
+use Neos\Neos\Controller\CreateContentContextTrait;
+use Neos\Flow\Http;
+use Neos\Flow\Mvc;
+use Neos\Flow\I18n\Service;
+use Neos\Flow\I18n\Locale;
+use Neos\Neos\Domain\Service\FusionService;
+
+class FusionRenderingService
+{
+    use CreateContentContextTrait;
+
+    /**
+     * @Flow\Inject
+     * @var Service
+     */
+    protected $i18nService;
+
+    /**
+     * @var Runtime
+     */
+    protected $fusionRuntime;
+
+    /**
+     * @Flow\Inject
+     * @var FusionService
+     */
+    protected $fusionService;
+
+    /**
+     * @var array
+     */
+    protected $options = ['enableContentCache' => true];
+
+    public function initializeObject() {
+        // we want to have nice URLs
+        putenv('FLOW_REWRITEURLS=1');
+    }
+
+    /**
+     * @param NodeInterface $node
+     * @param string $fusionPath
+     * @param array $contextData
+     * @return string
+     */
+    public function render(NodeInterface $node, $fusionPath, array $contextData = [])
+    {
+        $currentSiteNode = $node->getContext()->getCurrentSiteNode();
+        $fusionRuntime = $this->getFusionRuntime($currentSiteNode);
+
+        $dimensions = $node->getContext()->getDimensions();
+        if (array_key_exists('language', $dimensions) && $dimensions['language'] !== []) {
+            $currentLocale = new Locale($dimensions['language'][0]);
+            $this->i18nService->getConfiguration()->setCurrentLocale($currentLocale);
+            $this->i18nService->getConfiguration()->setFallbackRule(['strict' => false, 'order' => array_reverse($dimensions['language'])]);
+        }
+
+        $fusionRuntime->pushContextArray(array_merge([
+            'node' => $node,
+            'documentNode' => $this->getClosestDocumentNode($node) ?: $node,
+            'site' => $currentSiteNode,
+            'editPreviewMode' => null,
+        ], $contextData));
+        $output = $fusionRuntime->render($fusionPath);
+        $fusionRuntime->popContext();
+        return $output;
+    }
+
+    /**
+     * @param string $nodeIdentifier
+     * @param string $fusionPath
+     * @param string $workspace
+     * @param array $contextData
+     * @return string
+     */
+    public function renderByIdentifier(string $nodeIdentifier, string $fusionPath, string $workspace = 'live', array $contextData = []): string {
+        $context = $this->createContentContext($workspace);
+        $node = $context->getNodeByIdentifier($nodeIdentifier);
+        return $this->render($node, $fusionPath, $contextData);
+    }
+
+    /**
+     * @return Mvc\Controller\ControllerContext
+     */
+    protected function buildControllerContext()
+    {
+        $schemeAndHost = 'https://punkt.de/';
+
+        $httpRequest = Http\Request::create(new Http\Uri($schemeAndHost));
+        $httpRequest->setBaseUri(new Http\Uri($schemeAndHost));
+
+        return new Mvc\Controller\ControllerContext(
+            new Mvc\ActionRequest($httpRequest),
+            new Http\Response(),
+            new Mvc\Controller\Arguments(),
+            new Mvc\Routing\UriBuilder()
+        );
+    }
+
+    /**
+     * @param NodeInterface $currentSiteNode
+     * @return Runtime
+     */
+    protected function getFusionRuntime(NodeInterface $currentSiteNode)
+    {
+        if ($this->fusionRuntime === null) {
+            $this->fusionRuntime = $this->fusionService->createRuntime($currentSiteNode, $this->buildControllerContext());
+
+            if (isset($this->options['enableContentCache']) && $this->options['enableContentCache'] !== null) {
+                $this->fusionRuntime->setEnableContentCache($this->options['enableContentCache']);
+            }
+        }
+        return $this->fusionRuntime;
+    }
+
+    /**
+     * @param NodeInterface $node
+     * @return NodeInterface
+     */
+    protected function getClosestDocumentNode(NodeInterface $node)
+    {
+        while ($node !== null && !$node->getNodeType()->isOfType('Neos.Neos:Document')) {
+            $node = $node->getParent();
+        }
+        return $node;
+    }
+}
